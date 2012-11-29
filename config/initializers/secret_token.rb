d31194421cce599e435819a20ea619a1fcf4ac90
Logan Buesching <agolna@gmail.com>
2012-11-28 21:43:01 -0800

    Initial repository, with some good gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'c7f9c23de7ffdb29d57a621ef779385dd9134116164a4b13efb8086e1edd453f2917a5a3b6d97f984861e1b2537681428a224144d8f5b924a96a5b78394a302f'
